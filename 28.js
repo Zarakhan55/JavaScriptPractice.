dd376c21cfce951068140c88e9aa1b0b1c78580c
Zara Khan <zarakhang067@gmail.com>
2025-12-17 08:10:10 -0800

    Add files via upload

---
+++ 28.js
@@ -0,0 +1,12 @@
+//function trancaseString 
+
+function truncateString(str, num) {
+    if (str.length <= num) {
+        return str;
+    } else {
+        return str.slice(0, num) + '...';
+    }
+}
+
+console.log(truncateString("Hello, world!", 9));
+

108d9f8eb42b62fc52cdc4b2bc03d8ebcad41819
Zara Khan <zarakhang067@gmail.com>
2026-09-05 10:15:21 +0500

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,20 @@
+// for(let x=1;x<=100;x++){
+//     console.log("zara");
+// }
+
+// for (let x = 1; x <= 10; x++) {
+//     if (x !== 5) {
+//         console.log(x);
+//     }
+// }
+//  let fruits = ["apple", "mango", "banana", "cherry", "grapes", "watermelon", "Dates", "strawberry", "Guava", "papaya"];
+
+// for (let x = 0; x < 10; x++) {
+//     console.log(fruits[x]);
+// }
+
+let no=Number(prompt("enter table number"));
+for (let x=1;x<=10;x++){
+    console.log(no + "*" + x + "=" + no*x);
+
+}
